65ba0756941b4d5144b48ccc4356d1d9b157ad92
Ilia Shapovalov <greateman@mail.ru>
2023-08-30 13:20:51 +0300

    Initial Commit

---
+++ Workshop_30.08/Workshop_30_08App.swift
@@ -0,0 +1,17 @@
+//
+//  Workshop_30_08App.swift
+//  Workshop_30.08
+//
+//  Created by Илья Шаповалов on 30.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Workshop_30_08App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
